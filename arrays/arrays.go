f06626e844339665a8b3393478c298d1ddea955c
Yogesh <yogeshmaher64@gmail.com>
2020-08-07 10:13:47 +0530

    Arrays added

---
+++ arrays/arrays.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [5]int
+
+	fmt.Println("Array a", a)
+
+	a[4] = 100
+
+	fmt.Println("Array a", a)
+
+	fmt.Println("Length of and array a", len(a))
+
+	b := [4]int{1, 2, 3, 4}
+
+	fmt.Println("Array b", b)
+
+	var twoD [2][3]int
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("Two D array", twoD)
+
+}
