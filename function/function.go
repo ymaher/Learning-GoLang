1e1a6fe2af21961dba7cd254cd5762cf6fd8363e
Yogesh <yogeshmaher64@gmail.com>
2020-08-08 14:23:50 +0530

    function completed

---
+++ function/function.go
@@ -0,0 +1,22 @@
+// package main
+
+// import "fmt"
+
+// //function plus which take 2 arguments of type int and return of type int
+// func plus(a int, b int) int {
+// 	return a + b // Go does not return implictly
+// }
+
+// // functions with same type can omit the data type and can be mentioned after last argument
+// func plus1(a, b, c int) int {
+// 	return a + b + c
+// }
+
+// func main() {
+
+// 	res := plus(1, 2)
+// 	fmt.Println("1+2=", res)
+
+// 	result := plus1(1, 2, 3)
+// 	fmt.Println("1+2+3=", result)
+// }
