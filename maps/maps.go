127c18dbf82eed073a73a5443881bcb0f61b28cb
Yogesh <yogeshmaher64@gmail.com>
2020-08-08 12:01:10 +0530

    Maps completed

---
+++ maps/maps.go
@@ -0,0 +1,41 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	// Empty map initialization witn make bullitine
+// 	m := make(map[string]int)
+
+// 	fmt.Println("Empty Map:", m)
+
+// 	//adding key pair values
+// 	m["k1"] = 5
+// 	m["k2"] = 7
+// 	m["k3"] = 3
+
+// 	fmt.Println("Map m is:", m)
+
+// 	//assigning value of k2 to nV1 variable
+// 	v1 := m["k2"]
+
+// 	fmt.Println("Value of V1:", v1)
+
+// 	//length of map
+// 	fmt.Println("Length of Map m is:", len(m))
+
+// 	m["k4"] = 9
+
+// 	fmt.Println("Length of Map m is:", len(m))
+
+// 	//delete bullitin removes key value pair
+// 	delete(m, "k2")
+
+// 	fmt.Println("Map m after deletion K2 is:", m)
+
+// 	//Initializing a map with key value pair
+// 	n := map[string]int{"foo": 1, "bar": 2}
+
+// 	fmt.Println("Map n is as:", n)
+
+// }
