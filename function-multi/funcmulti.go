f95a7597acd098a2135f95c5dec16787de14107e
Yogesh <yogeshmaher64@gmail.com>
2020-08-09 00:31:59 +0530

    function multiple values completed

---
+++ function-multi/funcmulti.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func val() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	a, b := val()
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c, _ := val()
+
+	fmt.Println(c)
+
+}
