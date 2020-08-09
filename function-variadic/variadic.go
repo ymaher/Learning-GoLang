6bea7cfaf66f0b1de4d52b6836e7f675ccabb55c
Yogesh <yogeshmaher64@gmail.com>
2020-08-09 10:53:23 +0530

    function variadic updated

--- function-variadic/variadic.go
+++ function-variadic/variadic.go
@@ -1,25 +1,25 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func sum(nums ...int) { //Here’s a function that will take an arbitrary number of ints as arguments.
+// func sum(nums ...int) { //Here’s a function that will take an arbitrary number of ints as arguments.
 
-	fmt.Print(nums, " ")
+// 	fmt.Print(nums, " ")
 
-	total := 0
+// 	total := 0
 
-	for _, num := range nums {
-		total = total + num
-	}
-	fmt.Println(total)
-}
+// 	for _, num := range nums {
+// 		total = total + num
+// 	}
+// 	fmt.Println(total)
+// }
 
-func main() {
+// func main() {
 
-	sum(1, 2) //Variadic functions can be called in the usual way with individual arguments.
-	sum(1, 2, 3)
+// 	sum(1, 2) //Variadic functions can be called in the usual way with individual arguments.
+// 	sum(1, 2, 3)
 
-	nums := []int{1, 2, 3, 4} //If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this.
-	sum(nums...)
+// 	nums := []int{1, 2, 3, 4} //If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this.
+// 	sum(nums...)
 
-}
+// }
