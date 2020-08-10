5fb9ea26a02d1e22454e1df43be6fb7b1d62344e
Yogesh <yogeshmaher64@gmail.com>
2020-08-10 23:30:51 +0530

    Puzzle Game added

---
+++ Puzzle/puzzl.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	var number int
+
+	//r := rand.Intn(10) + 1
+
+	var res bool = true
+
+	for i := 1; res == true; i++ {
+
+		s := rand.NewSource(time.Now().UnixNano())
+		r := rand.New(s)
+		//fmt.Println("R", r)
+		//fmt.Println("S", s)
+		result := r.Intn(10)
+
+		fmt.Println("What is your fav number?")
+		fmt.Scanln(&number)
+
+		if result == number {
+			fmt.Println("Random no is", result)
+			fmt.Println("Yay! You got it!!!")
+			res = false
+			break
+
+		} else if i == 3 {
+			res = false
+			break
+		} else {
+
+			fmt.Println("Bad Luck, Try again")
+			fmt.Println("Random no is", result)
+			continue
+		}
+
+	}
+
+}
