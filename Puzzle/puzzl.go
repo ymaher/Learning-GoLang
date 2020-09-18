dc374dfa1d97ec6ea0b123d44590b3415adcf9ff
Yogesh <yogeshmaher64@gmail.com>
2020-09-18 12:54:32 +0530

    Puzzle added

--- Puzzle/puzzl.go
+++ Puzzle/puzzl.go
@@ -1,47 +1,47 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
-func main() {
+// func main() {
 
-	var number int
+// 	var number int
 
-	//r := rand.Intn(10) + 1
+// 	//r := rand.Intn(10) + 1
 
-	var res bool = true
+// 	var res bool = true
 
-	for i := 1; res == true; i++ {
+// 	for i := 1; res == true; i++ {
 
-		s := rand.NewSource(time.Now().UnixNano())
-		r := rand.New(s)
-		//fmt.Println("R", r)
-		//fmt.Println("S", s)
-		result := r.Intn(10)
+// 		s := rand.NewSource(time.Now().UnixNano())
+// 		r := rand.New(s)
+// 		//fmt.Println("R", r)
+// 		//fmt.Println("S", s)
+// 		result := r.Intn(10)
 
-		fmt.Println("Guess the number?")
-		fmt.Scanln(&number)
+// 		fmt.Println("Guess the number?")
+// 		fmt.Scanln(&number)
 
-		if result == number {
-			//fmt.Println("Random no is", result)
-			fmt.Println("Yay! You got it!!!")
-			res = false
-			break
+// 		if result == number {
+// 			//fmt.Println("Random no is", result)
+// 			fmt.Println("Yay! You got it!!!")
+// 			res = false
+// 			break
 
-		} else if i == 3 {
-			fmt.Println("Game Over!")
-			res = false
-			break
-		} else {
+// 		} else if i == 3 {
+// 			fmt.Println("Game Over!")
+// 			res = false
+// 			break
+// 		} else {
 
-			fmt.Println("Bad Luck, Try again")
-			//fmt.Println("Random no is", result)
-			continue
-		}
+// 			fmt.Println("Bad Luck, Try again")
+// 			//fmt.Println("Random no is", result)
+// 			continue
+// 		}
 
-	}
+// 	}
 
-}
+// }
